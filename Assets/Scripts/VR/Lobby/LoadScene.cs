0d4a54021f919441b7dbb7a5cafa25e27c86527c
Audun Wigum Arbo <audunwar@stud.ntnu.no>
2017-12-30 16:19:06 +0100

    Add very simple lobby scene

---
+++ Assets/Scripts/VR/Lobby/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Provides functionality to load a scene provided from the inspector
+/// </summary>
+public class LoadScene : MonoBehaviour {
+    [SerializeField] private SceneAsset scene;
+	
+    public void Load()
+    {
+        SceneManager.LoadScene(scene.name, LoadSceneMode.Single);
+    }
+
+}
