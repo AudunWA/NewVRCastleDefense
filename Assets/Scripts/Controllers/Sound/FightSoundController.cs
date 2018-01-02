47d23145cdf25d2b8be7b3bc8d8a235cca2c08cd
Elias <eliassorr@gmail.com>
2018-01-02 21:24:00 +0100

    Added sounds for arrows. Finalized sound levels. First sound demo done

--- Assets/Scripts/Controllers/Sound/FightSoundController.cs
+++ Assets/Scripts/Controllers/Sound/FightSoundController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FightSoundController : MonoBehaviour {
+
     public AudioClip punchSound;
     public AudioClip swooshSound;
     public AudioClip fistpunchSound;
@@ -20,10 +21,6 @@ public class FightSoundController : MonoBehaviour {
     public void PlayRandomFightSound()
     {        
         audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Count)], 1f);
-        /*int x = Random.Range(0, 3);
-        if(x == 0)audioSource.PlayOneShot(punchSound);
-        else if(x==1)audioSource.PlayOneShot(swooshSound);
-        else audioSource.PlayOneShot(fistpunchSound);*/
     }
     // Use this for initialization
     void Start ()
