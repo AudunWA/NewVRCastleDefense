4b452df237b8f39a20228fee0c773123007eeb21
Christian Echtermeyer <chrech.birr@gmail.com>
2018-01-02 14:59:44 +0100

    rain arrow

---
+++ Assets/Scripts/VR/Weapons/DuplicateArrows.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuplicateArrows : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("Duplicate", 3.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Duplicate()
+    {
+        Instantiate(gameObject, new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y + 1, gameObject.transform.position.z + 1), gameObject.transform.rotation);
+    }
+}
