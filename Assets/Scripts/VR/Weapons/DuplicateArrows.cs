0972ddfcba5ca9f93a9f05747819110762f06b4b
VRCastleDefense <awa500@hotmail.com>
2018-01-02 17:19:32 +0100

    arrows just fall out of the sky, some weird feature

--- Assets/Scripts/VR/Weapons/DuplicateArrows.cs
+++ Assets/Scripts/VR/Weapons/DuplicateArrows.cs
@@ -1,21 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
-public class DuplicateArrows : MonoBehaviour {
-
+public class DuplicateArrows : MonoBehaviour
+{
+	public GameObject copy;
+	
 	// Use this for initialization
 	void Start () {
-		Invoke("Duplicate", 3.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		
+	}
+
+	void ArrowFired()
+	{
+		Debug.Log("fired");
+		Invoke(nameof(Duplicate), 0.5f);
 	}
 
     void Duplicate()
     {
-        Instantiate(gameObject, new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y + 1, gameObject.transform.position.z + 1), gameObject.transform.rotation);
+        GameObject go = Instantiate(copy, new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y + 1, gameObject.transform.position.z + 1), gameObject.transform.rotation);
+	    go.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity.normalized * 10;
     }
 }
