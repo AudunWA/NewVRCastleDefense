b273f526ea912762f24c3a8a4c9c859a7c60e6ae
Ole Kristian Aune <okaune@outlook.com>
2018-01-05 02:48:12 +0100

    Map length shortend, width of upgrade ui shortened and some other tweaks

--- Assets/Scripts/Controllers/UIMapOverlayController.cs
+++ Assets/Scripts/Controllers/UIMapOverlayController.cs
@@ -7,6 +7,10 @@ public class UIMapOverlayController : MonoBehaviour {
 
 	GameObject Money;
 	Player Player;
+	public Image EnemyCastleHealthBar;
+	public Text EnemyCastleHealthText;
+	public Image MyCastleHealthBar;
+	public Text MyCastleHealthText;
 
 	// Use this for initialization
 	void Start () {
@@ -17,5 +21,7 @@ public class UIMapOverlayController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Money.GetComponent<Text>().text = "$" + Player.Money;
+		EnemyCastleHealthText.text = Mathf.Floor(EnemyCastleHealthBar.fillAmount * 100) + "/100";
+		MyCastleHealthText.text = Mathf.Floor(MyCastleHealthBar.fillAmount * 100) + "/100";
 	}
 }
